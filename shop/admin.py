74e87242cb3227c00fe6182508e59ca4f2653274
unknown <davitpapidze777@gmail.com>
2024-12-26 22:00:05 +0400

    davaleba 11

--- shop/admin.py
+++ shop/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from shop.models import Category, Tag, Item
 
-# Register your models here.
+class ItemInline(admin.TabularInline0):
+    model = Item
+
+class CategoryManager(admin.ModelAdmin):
+    list_display = ['name', 'description']
+    search_fields = ['name', 'description']
+    ordering = ['name']
+    inlines = [ItemInline]
+
+class TagInLine(admin.StackedInline):
+    model = Tag
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['name', 'category', 'price', 'description']
+    search_fields = ['name', 'description']
+    ordering = ['-price']
+    inlines = [TagInLine]
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
